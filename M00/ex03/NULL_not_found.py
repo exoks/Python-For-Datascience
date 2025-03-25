24407e8c27a86a0c6b7de440189134313d19d354
Ous Sama <oussamaezzaou@gmail.com>
2025-03-25 18:18:07 +0000

    M00: ex03: is done

--- M00/ex03/NULL_not_found.py
+++ M00/ex03/NULL_not_found.py
@@ -5,12 +5,39 @@
 #  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
 #  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
 #  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/03/24 18:05:53 by oezzaou
-#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/03/24 18:12:17 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/03/25 18:17:38 by oezzaou
 #  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
 #  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
 #  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
 #  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
 #  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
 
+import math
+
+#===[ NULL_nof_found: ]=========================================================
 def NULL_not_found(object: any) -> int:
-  return (0)
+  myDict = {
+    "Nothing": None,
+    "Cheese": float('NaN'),
+    "Fake": False,
+    "Empty": "",
+    "Zero": 0,
+  }
+
+  for key, value in myDict.items():
+    if (object == value) or (value.__class__ == float and object != object):
+      print(f"{key}: {value} {value.__class__}")
+      return (0)
+  print(f"Type not Found");
+  return (1)
+
+
+# NOTE:=========================================================================
+# - NaN is value used by IEEE floating-point for undefined things, ex: 0/0, ...|
+# - This EX is about NULL types in python.                                     |
+# - Sinse NULL does not exist in term of NULL pointers in C/C++, Python depends|
+#   on NULL object instead, An object 'None' that does not have attributes     |
+# - Python abstructs pointers, None obj is great idea to define the presense   |
+#   value                                                                      |
+# - NaN is not equal to NaN.                                                   |
+# ==============================================================================
